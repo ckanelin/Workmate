61f9a12491b1d8805dc52e2e8d5c684ea4e7d525
Yan <yyzhang@student.ubc.ca>
2020-12-06 17:09:21 -0800

    resolve conflicts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, {Component} from 'react';
+import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
 import CardTask from "./TaskCard.js";
 
 function App() {
@@ -9,68 +8,6 @@ function App() {
         <CardTask/>
       </header>
   );
-=======
-import SignIn from './Components/SignIn';
-import Register from './Components/Register';
-import DashBoard from './Components/Dashboard';
-
-const initialState = {
-  route: 'signIn',
-}
-
-class App extends Component {
-
-  constructor(){
-    super();
-    this.state = initialState;
-  }
-
-  onChangeRoute = (route) =>{
-    if(route !== 'signOut'){
-      this.setState({route: route});
-    }else{
-      this.setState(initialState);
-    }
-  }
-
-  onLogOut = (route) => {
-    this.setState({route: 'signIn'})
-  }
-
-  render(){
-    const {route} = this.state;
-
-    return (
-      <div>
-        {
-          route === 'home' ?
-          <div>
-             <DashBoard onLogOut={this.onLogOut}/>
-          </div>:
-
-          <div className="flex">
-            <div className="w-50 vh-100 bg-light-blue">
-            </div>
-
-            <div className="w-50 vh-100 flex flex-column justify-center items-center">
-                {
-                    route === 'signIn'?
-                    <SignIn 
-                      onChangeRoute={this.onChangeRoute}
-                    /> :
-          
-                    <Register 
-                      onChangeRoute={this.onChangeRoute} 
-                    /> 
-                }
-            </div>
-          </div>
-        }
-      </div>
-    );
-  }
-
->>>>>>> upstream/master
 }
 
 export default App;
